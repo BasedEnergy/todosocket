ed81b0c8ce8b2558cd4203427fec0e9390596e86
basedenergy <hollindgolden@gmailcom>
2018-11-14 01:37:15 -0500

    set up for use with SQL db

--- public/js/app.js
+++ public/js/app.js
@@ -1,85 +1,132 @@
-$(function () {
+$(document).ready(function () {
+    // Getting a reference to the input field where user adds a new todo
+    var $newItemInput = $("input.new-item");
+    // Our new todos will go inside the todoContainer
+    var $todoContainer = $(".todo-container");
+    // Adding event listeners for deleting, editing, and adding todos
+    $(document).on("click", "button.delete", deleteTodo);
+    $(document).on("click", "button.complete", toggleComplete);
+    $(document).on("click", ".todo-item", editTodo);
+    $(document).on("keyup", ".todo-item", finishEdit);
+    $(document).on("blur", ".todo-item", cancelEdit);
+    $(document).on("submit", "#todo-form", insertTodo);
 
-    const state = {
-        todoList: [],
-        baseUrl: 'https://devin-todosocket.herokuapp.com/'
-    };
+    // Our initial todos array
+    var todos = [];
 
-    const render = function () {
+    // Getting todos from database when page loads
+    getTodos();
 
-        $.ajax({
-            url: state.baseUrl + '/api/data',
-            type: 'GET',
-            success: function (data) {
-                state.todoList = data;
+    // This function resets the todos displayed with new todos from the database
+    function initializeRows() {
+        $todoContainer.empty();
+        var rowsToAdd = [];
+        for (var i = 0; i < todos.length; i++) {
+            rowsToAdd.push(createNewRow(todos[i]));
+        }
+        $todoContainer.prepend(rowsToAdd);
+    }
 
-                $('#list').empty();
-
-                state.todoList.forEach((li, i) =>
-                    $('#list').append(
-                        $('<li>').attr('id', 'item' + i).append([
-                            $('<input>').attr({ type: 'checkbox', id: i + li._id, class: 'check', checked: (li.checked === 'true') }),
-                            $('<label>').attr({ for: 'li' + i, class: 'lbl' }).append(li.label),
-                            $('<button>').attr({ id: li._id + i, class: 'delete' }).append(
-                                $('<i>').attr('class', 'fas fa-times')
-                            )
-                        ])
-                    )
-                );
-            }
+    // This function grabs todos from the database and updates the view
+    function getTodos() {
+        $.get("/api/todos", function (data) {
+            todos = data;
+            initializeRows();
         });
-    };
+    }
 
+    // This function deletes a todo when the user clicks the delete button
+    function deleteTodo(event) {
+        event.stopPropagation();
+        var id = $(this).data("id");
+        $.ajax({
+            method: "DELETE",
+            url: "/api/todos/" + id
+        }).then(getTodos);
+    }
 
-    $('#addbutton').on('click', function () {
+    // This function handles showing the input box for a user to edit a todo
+    function editTodo() {
+        var currentTodo = $(this).data("todo");
+        $(this).children().hide();
+        $(this).children("input.edit").val(currentTodo.text);
+        $(this).children("input.edit").show();
+        $(this).children("input.edit").focus();
+    }
 
-        const newItem = {
-            label: $('#input').val().trim(),
-            checked: false
+    // Toggles complete status
+    function toggleComplete(event) {
+        event.stopPropagation();
+        var todo = $(this).parent().data("todo");
+        todo.complete = !todo.complete;
+        updateTodo(todo);
+    }
+
+    // This function starts updating a todo in the database if a user hits the "Enter Key"
+    // While in edit mode
+    function finishEdit(event) {
+        var updatedTodo = $(this).data("todo");
+        if (event.which === 13) {
+            updatedTodo.text = $(this).children("input").val().trim();
+            $(this).blur();
+            updateTodo(updatedTodo);
+        }
+    }
+
+    // This function updates a todo in our database
+    function updateTodo(todo) {
+        $.ajax({
+            method: "PUT",
+            url: "/api/todos",
+            data: todo
+        }).then(getTodos);
+    }
+
+    // This function is called whenever a todo item is in edit mode and loses focus
+    // This cancels any edits being made
+    function cancelEdit() {
+        var currentTodo = $(this).data("todo");
+        if (currentTodo) {
+            $(this).children().hide();
+            $(this).children("input.edit").val(currentTodo.text);
+            $(this).children("span").show();
+            $(this).children("button").show();
+        }
+    }
+
+    // This function constructs a todo-item row
+    function createNewRow(todo) {
+        var $newInputRow = $(
+            [
+                "<li class='list-group-item todo-item'>",
+                "<span>",
+                todo.text,
+                "</span>",
+                "<input type='text' class='edit' style='display: none;'>",
+                "<button class='delete btn btn-danger'>x</button>",
+                "<button class='complete btn btn-primary'>✓</button>",
+                "</li>"
+            ].join("")
+        );
+
+        $newInputRow.find("button.delete").data("id", todo.id);
+        $newInputRow.find("input.edit").css("display", "none");
+        $newInputRow.data("todo", todo);
+        if (todo.complete) {
+            $newInputRow.find("span").css("text-decoration", "line-through");
+        }
+        return $newInputRow;
+    }
+
+    // This function inserts a new todo into our database and then updates the view
+    function insertTodo(event) {
+        event.preventDefault();
+        var todo = {
+            text: $newItemInput.val().trim(),
+            complete: false
         };
 
-        $.ajax({
-            url: state.baseUrl + '/api/data',
-            type: 'POST',
-            data: { item: newItem },
-            success: function (done) {
-                if (done)
-                    render();
-            }
-        });
-    });
-
-
-    $('#list').on('click', '.delete', function () {
-        let ID = $(this).attr('id');
-        ID = ID.slice(0, ID.length - 1);
-
-        $.ajax({
-            url: state.baseUrl + '/api/data',
-            type: 'DELETE',
-            data: { _id: ID },
-            success: function (done) {
-                if (done)
-                    render();
-            }
-        });
-    });
-
-
-    $('#list').on('click', '.check', function () {
-        let ID = $(this).attr('id');
-        ID = ID.slice(1, ID.length);
-
-        $.ajax({
-            url: state.baseUrl + '/api/data',
-            type: 'PUT',
-            data: { _id: ID, checked: $(this).is(':checked') },
-            success: function (done) {
-                if (done)
-                    render();
-            }
-        });
-    });
-
-    render();
+        $.post("/api/todos", todo, getTodos);
+        $newItemInput.val("");
+    }
 });
--- server.js
+++ server.js
@@ -1,52 +1,24 @@
-// Imports express into our info.app and sets it up for use
-const express = require('express');
+var express = require("express");
+var bodyParser = require("body-parser");
 
-var info = {
-    app: undefined,
-    db: undefined,
-    COLLECTION: undefined,
-    ObjectID: undefined,
-    Item: undefined
-};
+var app = express();
+var PORT = process.env.PORT || 8080;
 
-//MongoDB requirements
-const mongoose = require('mongoose');
-info.ObjectID = mongoose.Types.ObjectId;
-info.COLLECTION = 'todolist';
-const MONGODB_URI = 'mongodb://basedenergy:mongo123@ds119853.mlab.com:19853/heroku_t5q5kmg3';
+var db = require("./models");
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
+app.use(express.static("public"));
 
-info.app = express();
+require("./routes/api-routes.js")(app);
 
-// make express look in the public directory for assets (css/js/img)
-info.app.use(express.static(__dirname + '/public'));
-
-
-const bodyParser = require('body-parser');
-// Sets up our server to parse our request body for usage
-info.app.use(bodyParser.json());
-info.app.use(bodyParser.urlencoded({ extended: true }));
-
-// Routes
-// -----------------
-
-require('./routes/routes.js')(info);
-
-// Connect to the database before starting the info.application server.
-mongoose.connect(MONGODB_URI,{ useNewUrlParser: true }, function (err, database) {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
-
-    info.db = database;
-
-    console.log("Database connection ready");
-
-    // Starts our server on the predefined PORT
-    //Using process.env.PORT to allow Heroku to dynamically choose its own port
-    info.app.listen(process.env.PORT || 8080, function(){
-        console.log(`app is now listening on PORT `+ process.env.PORT);
+db.sequelize.sync({
+    force: true
+}).then(function () {
+    app.listen(PORT, function () {
+        console.log("App listening on PORT " + PORT);
     });
 });
